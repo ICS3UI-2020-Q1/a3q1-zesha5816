a5f3a38d7eba4a329e0716a16d57d4e22bde5bbd
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 11:34:09 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *  a program that will find the maximum integer between three numbers that the user inputs.
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+   
+	//prompts user for the three integer
+	System.out.println("Please enter three integers on separate lines");
+	//gets the input from user on seperate line
+	int num1 = input.nextInt();
+	int num2 = input.nextInt();
+  int num3 = input.nextInt();  
+	//calculates the biggest number and prints it respectly
+	if(num2 < num1 && num1 > num3){
+		System.out.println("Maximum: " + num1);
+	}else if (num1 < num2 && num2 > num3){
+		System.out.println("Maximum: " + num2);
+	}else{
+		System.out.println("Maximum: " + num3);
+	}
   }
 }
